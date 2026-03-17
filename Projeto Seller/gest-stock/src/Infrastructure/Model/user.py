f23d2ed098fc4668b567be1b062b84cf686e988d
Heitor dos Santos <157070090+Hocrons@users.noreply.github.com>
2026-03-17 00:04:11 -0300

    Adicionando a base

---
+++ Projeto Seller/gest-stock/src/Infrastructure/Model/user.py
@@ -0,0 +1,21 @@
+from src.config.data_base import db 
+class User(db.Model):
+    __tablename__ = 'users'
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(100), nullable=False)
+    email = db.Column(db.String(100), unique=True, nullable=False)
+    cnpj = db.Column(db.String(14), unique=True, nullable=False)
+    celular = db.Column(db.String(20), unique=True, nullable=False)
+    password = db.Column(db.String(100), nullable=False)
+    status = db.Column(db.String(20), nullable=False, default='inativo')
+
+    def to_dict(self):
+        return {
+            "id": self.id,
+            "name": self.name,
+            "email": self.email,
+            "password": self.password,
+            "status": self.status,
+            "celular": self.celular,
+            "cnpj": self.cnpj
+        }
